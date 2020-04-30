7cd6abcb18032f2cc6c460daa7d2e9cfbff09664
rriabets@deloitte.com <rriabets@deloitte.com>
2020-04-30 13:50:19 +0300

    Ui improvments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,10 +29,11 @@ class PostsController < ApplicationController
     @post.photo.attach(post_params[:photo])
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to profiles_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
         ActionCable.server.broadcast "feed_channel",
                                      content: get_channel_message
+
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
