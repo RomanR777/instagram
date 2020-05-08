b567f00b3add3c363594cb7245f606daa2711a79
rriabets@deloitte.com <rriabets@deloitte.com>
2020-05-08 17:26:27 +0300

    fixed tests according comments

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname { FFaker::Internet.user_name }
+    nickname { "___#{FFaker::Internet.user_name}___" }
     email { FFaker::Internet.email }
     password { FFaker::Internet.password }
   end
